f75220e0e8b444a7687ddf535da8c495beb94d43
Rahul Manavalan <rahul.manavalan@tum.de>
2021-08-11 18:35:51 +0200

    New Scope timer for measurement

--- Tensor-Networks/utilities.h
+++ Tensor-Networks/utilities.h
@@ -2,10 +2,14 @@
 #include<unordered_map>
 #include<string> 
 #include<vector> 
+#include<chrono> 
+#include<numeric>
++
+
+static std::unordered_map<std::string,double> statistics; 
 
 namespace Utilities
 {
-
 /**
  * 
  * Cumulative  product 
@@ -37,9 +41,33 @@ T cumulative_product(const std::vector<T>& vec)
     class ScopeTimer
     {
     public: 
+        ScopeTimer(std::string name)
+        {
+            function_name = name; 
+            start = std::chrono::high_resolution_clock::now(); 
+        }
 
+        ~ScopeTimer()
+        {
+            end = std::chrono::high_resolution_clock::now(); 
+            auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+            statistics[function_name].push_back(elapsed_time.count()); 
+        }
     private:
-
+        std::string function_name; 
+        std::chrono::time_point start; 
+        std::chrono::time_point end; 
+        static std::unordered_map<std::string ,std::vector<double>> statistics; 
     };
 
+
+ // Print the total time spent in all of the functions 
+// We could either be satisfied with such an approximation or take up a more challenging setup and try to compute the better Statistics 
+    void print_statistics()
+    {
+        std::cerr <<  "Time spent in each of the subroutines " << std::endl; 
+        for(std::pair<std::string , std::vector<double>> stat : statistics) {
+            std::cerr << stat.first << " : " << std::accumulate(stat.begin(),stat.end(),0) << "\n"; 
+        }
+    }
 };
